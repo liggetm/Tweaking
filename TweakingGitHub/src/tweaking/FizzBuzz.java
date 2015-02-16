c2bea0ee0c483991bb419cfd9d6790c652821d7e
ml2387 <ml2387@my.open.ac.uk>
2015-02-16 18:06:35 +0000

    Add fizzbuzz for fun

---
+++ TweakingGitHub/src/tweaking/FizzBuzz.java
@@ -0,0 +1,27 @@
+package tweaking;
+
+/**
+ *
+ * @author mliggett
+ */
+public class FizzBuzz {
+
+    public static void main(final String[] args) {
+
+        for (int i = 1; i < 100; i++) {
+
+            if (i % 15 == 0) {
+                System.out.println("fizzbuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
+            }
+
+        }
+
+    }
+
+}
--- TweakingGitHub/src/tweaking/Tweaking.java
+++ TweakingGitHub/src/tweaking/Tweaking.java
@@ -1,23 +1,29 @@
 package tweaking;
 
-import org.apache.commons.lang.StringUtils;
-
 public class Tweaking {
 
     public static void main(String[] args) {
 
-        final String sourceBicPath = "1.2.3.4 7/8/9";
-        final String deviceIp = StringUtils.split(sourceBicPath, ' ')[0];
-        final String[] bicPathComponents = StringUtils.split(sourceBicPath.substring(sourceBicPath.lastIndexOf(' ') + 1), '/');
-
-        final int shelfIdx = Integer.parseInt(bicPathComponents[0]);
-        final int slotIdx = Integer.parseInt(bicPathComponents[1]);
-        final int bicIdx = Integer.parseInt(bicPathComponents[2]);
+//        final String sourceBicPath = "1.2.3.4 7/8/9";
+//        final String deviceIp = StringUtils.split(sourceBicPath, ' ')[0];
+//        final String[] bicPathComponents = StringUtils.split(sourceBicPath.substring(sourceBicPath.lastIndexOf(' ') + 1), '/');
+//
+//        final int shelfIdx = Integer.parseInt(bicPathComponents[0]);
+//        final int slotIdx = Integer.parseInt(bicPathComponents[1]);
+//        final int bicIdx = Integer.parseInt(bicPathComponents[2]);
+//        
+//        System.out.println("deviceIp = " + deviceIp);
+//        System.out.println("shelf = " + shelfIdx);
+//        System.out.println("slot = " + slotIdx);
+//        System.out.println("bic = " + bicIdx);
+        final String out = generateDolMuxPort("md:0/99", "L1");
+        System.out.println("out = " + out);
         
-        System.out.println("deviceIp = " + deviceIp);
-        System.out.println("shelf = " + shelfIdx);
-        System.out.println("slot = " + slotIdx);
-        System.out.println("bic = " + bicIdx);
+    }
+    
+    private static String generateDolMuxPort(final String muxEquipment, final String muxPort) {
+        final String santitizedEquipment = muxEquipment.substring(muxEquipment.indexOf(':'));
+        return "port" + santitizedEquipment + "/0/" + muxPort;
     }
 
 }
